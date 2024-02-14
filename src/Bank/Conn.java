0b69a50bf62bc8366facc3424acdc912d06e128d
Yadnesh Krishnakant Bhanushali <122076727+yadnesh13@users.noreply.github.com>
2024-02-15 00:53:45 +0530

    Add files via upload

--- src/Bank/Conn.java
+++ src/Bank/Conn.java
@@ -1,21 +1,21 @@
 package Bank;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Conn {
-
-
     Connection c;
     Statement s;
-    public Conn() {
 
+    public Conn() {
         try {
-            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","yadneshprime6942");
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","Yadnesh@#1303");
             s = c.createStatement();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-
     }
-
 }
